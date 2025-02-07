0565e94ce1d31e483def6516cf6ef1726c74dacc
Jay100202 <bhailumandaliya11@gmail.com>
2025-02-07 20:34:06 +0530

    refactor: migrate server code to functions directory and update Netlify configuration

--- function/server.js
+++ function/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const timeslotRoutes = require('./routes/timeslotRoutes');
-const errorHandler = require('./middleware/errorHandler');
+const connectDB = require('../config/db');
+const authRoutes = require('../routes/authRoutes');
+const timeslotRoutes = require('../routes/timeslotRoutes');
+const errorHandler = require('../middleware/errorHandler');
 
 const app = express();
 
